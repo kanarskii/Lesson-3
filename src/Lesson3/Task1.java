9133f1f374f628362ae0fbb5b85fa9d9a686dfce
kanarskievgeni <kanarskiievgenii198@gmail.com>
2024-03-27 23:15:45 +0300

    version-2

--- src/Lesson3/Task1.java
+++ src/Lesson3/Task1.java
@@ -3,5 +3,8 @@ package Lesson3;
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        for (int i = 0; i < 10; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
