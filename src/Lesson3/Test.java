507bcf2df74e252beda9cc578dcbfeaf3a519ca0
kanarskievgeni <kanarskiievgenii198@gmail.com>
2024-03-27 23:36:43 +0300

    version-5

--- src/Lesson3/Test.java
+++ src/Lesson3/Test.java
@@ -3,5 +3,8 @@ package Lesson3;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Привет из второй ветки");
+        for (int i = 10; i < 15; i++) {
+            System.out.print(i + " ");
+        }
     }
 }
